2a6e507df7169503ab12aa7450e8fe057c7feb7a
이영재 <dudwo1830@naver.com>
2024-07-02 00:03:04 +0900

    물방울 클릭

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero, 0f);
+            if (hit.collider != null)
+            {
+                WaterDropBase waterDrop = hit.transform.gameObject.GetComponent<WaterDropBase>();
+                if (waterDrop != null)
+                {
+                    waterDrop.Explode();
+                }
+            }
+        }
+    }
+}
